b37f1c0d93ec81a7fde75cf54b2ef79a8f04faeb
Maddy Long <madeline.long.qr@gmail.com>
2019-11-19 16:49:55 +1100

    Fixed authentication on pl index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,9 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
   include Pundit
-  after_action :verify_authorized, except: :index, unless: :skip_pundit?
-  after_action :veriy_policy_scoped, only: :index, unless: :skip_pundit?
+
+  after_action :verify_authorized, except: [:index, :home], unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: [:index, :home], unless: :skip_pundit?
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
