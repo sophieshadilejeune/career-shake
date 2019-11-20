1ffafae5ef011a3dfb87cd8386152dc8719de343
Ameya Akhouri <ameya.akhouri@gmail.com>
2019-11-20 13:45:33 +1100

    add connection create and delete functionality

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -1,4 +1,6 @@
 class Connection < ApplicationRecord
   belongs_to :user
   belongs_to :prof_listing
+
+  has_many :messages, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'pages#home'
-  resources :prof_listings, only: [:index, :show, :new, :create]
+  resources :prof_listings, only: [:index, :show, :new, :create] do
+    resources :connections, only: [:create, :destroy]
+
+  end
+
+  # resources :connections, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
