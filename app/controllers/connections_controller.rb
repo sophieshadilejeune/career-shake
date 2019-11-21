581a96aafe8d72e01ed294b008baf1adf3aa1855
Sophie Lejeune <sophie.shadi.lejeune@gmail.com>
2019-11-21 14:06:06 +1100

    added add connection button from show page and added confirmation messages

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -5,14 +5,19 @@ class ConnectionsController < ApplicationController
     @connection = Connection.new
     @connection.user = current_user
     @connection.prof_listing = ProfListing.find(params[:prof_listing_id])
-    @connection.save
-    redirect_to prof_listing_path(@connection.prof_listing)
-    authorize @connection
+    if @connection.save
+      flash[:alert] = "You added a new connection"
+      redirect_to prof_listing_path(@connection.prof_listing)
+      authorize @connection
+    else
+      redirect_to prof_listings_path
+    end
   end
 
   def destroy
     @connection = current_user.connections.where(prof_listing: ProfListing.find(params[:prof_listing_id])).last
     @connection.destroy
+    flash[:alert] = "You deleted a connection"
     authorize @connection
     redirect_to prof_listings_path
   end
