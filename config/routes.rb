d7b4d5e68c61b249761bdf5934a1a3e0eb0db6ae
Ameya Akhouri <ameya.akhouri@gmail.com>
2019-11-21 22:55:29 +1100

    add backend for dashboard and all required links, routes and methods

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
 
-get "prof_listings/test" => "prof_listings#test"
   root to: 'pages#home'
+
+  get 'dashboard', to: 'pages#dashboard'
+
+
   resources :prof_listings, only: [:index, :show, :new, :create, :edit, :update] do
     resources :connections, only: [:create, :destroy]
 
